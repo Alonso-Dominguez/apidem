4539c204a3bf81982062fb762b1f94287f60162e
Alonso-Dominguez Castillo <1722110219@utectulancingo.edu.mx>
2023-10-02 15:56:06 +0000

    Actualizacion

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 
 
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+import csv
 
 app = FastAPI()
+
+class Contacto(BaseModel):
+    nombre: str
+    email: str
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello":"World"}
 
 
 @app.get("/v1/contactos")
-async get_contactos():
-    # TODO read contactos.csv
-    # TODO JSON encode contactos 
-    # TODO save in response y mostrar la URI
-    response = []
-    return response
+def get_contactos():
+    datos = []
+    with open('contactos.csv', 'r') as file:
+        fildnames = ('nombre', 'email')
+        lector = csv.DictReader(file, fildnames)
+        for row in lector:
+            datos.append(row)
+    return datos
+
+@app.post("/v1/contactos")
+def create_contacto(contacto: Contacto):
+    with open('contactos.csv', 'a', newline='') as file:
+        fieldnames = ['nombre', 'email']
+        escritor = csv.DictWriter(file, fieldnames=fieldnames)
+        escritor.writerow({'nombre': contacto.nombre, 'email': contacto.email})
+    return {"mensaje": "Contacto creado exitosamente"}
